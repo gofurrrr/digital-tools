73608286fc5257067fb967813862ba6d13b810ac
gofurrrr <gofur0768@gmail.com>
2026-03-31 18:14:59 +0600

    codded about section and fix some previous code

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -24,7 +24,7 @@ const banner = () => {
             <button className="px-8 py-3 bg-[#8B1CFF] text-white font-semibold rounded-full hover:bg-[#6a12c7] hover:scale-105 transition-all shadow-lg shadow-purple-200 active:scale-95">
               Explore Products
             </button>
-            <button className="flex items-center gap-2 px-11 py-3  border-2 border-purple-200 text-purple-600 font-semibold rounded-full hover:bg-purple-500 hover:text-white hover:border-purple-600 transition-all active:scale-95 animate-pulse hover:scale-105">
+            <button className="flex items-center gap-2 px-11 py-3  border-2 border-purple-200 text-purple-600 font-semibold rounded-full hover:bg-purple-100  hover:text-white hover:border-purple-400 transition-all active:scale-95 animate-pulse hover:scale-105">
               Watch Demo
             </button>
           </div>
